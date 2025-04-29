3b9bd42003dd3515d7f47ab57122890e1b56c950
abimaelh <abimaelh@uw.edu>
2025-04-29 00:37:42 -0700

    update summarize

--- summarize.py
+++ summarize.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 import torch
 
 model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
-pipeline = transformers.pipeline("text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto")
+pipeline = pipeline("text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto")
 
 messages = [
     {"role": "system", "content": "You are a chatbot with expertise in summarizing documents"},
